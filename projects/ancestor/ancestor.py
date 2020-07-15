0265c262fc6effb9cc91b911f55c9e9d0be4ec0d
ToddMG <munyontodd@gmail.com>
2020-07-15 13:48:07 -0700

    Graphs Day 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,35 @@
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    tree = Graph()
+
+    for pair in ancestors:
+        if pair[0] not in tree.vertices:
+            tree.add_vertex(pair[0])
+        if pair[1] not in tree.vertices:
+            tree.add_vertex(pair[1])
+
+        tree.add_edge(pair[0], pair[1])
+
+
+    relations = []
+
+    for parent in tree.vertices:
+        path = tree.bfs(parent, starting_node)
+
+        if path is not None:
+            relations.append(path)
+
+    if len(relations) > 1:
+
+        oldest = relations[0]
+        for path in relations:
+            if len(path) > len(oldest):
+                oldest = path
+            if len(path) == len(oldest):
+                if path[0] < oldest[0]:
+                    oldest = path
+
+        return oldest[0]
+    else:
+        return -1
---
+++ projects/ancestor/graph.py
@@ -0,0 +1,234 @@
+"""
+Simple graph implementation
+"""
+from util import Stack, Queue  # These may come in handy
+
+class Graph:
+
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        if v1 and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        elif v1 not in self.vertices:
+            raise IndexError(f'Vertex {v1} does not exist.')
+        elif v2 not in self.vertices:
+            raise IndexError(f'Vertex {v2} does not exist.')
+        pass  # TODO
+
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        return self.vertices[vertex_id]
+
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        q = Queue()
+        q.enqueue(starting_vertex)
+        checked = set()
+
+        while q.size() > 0:
+            current_node = q.dequeue()
+
+            # if node hasn't been checked
+            if current_node not in checked:
+                print(current_node)
+                checked.add(current_node)
+                neighbors = self.get_neighbors(current_node)
+                for neighbor in neighbors:
+                    q.enqueue(neighbor)
+
+    def dft(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        """
+        s = Stack()
+
+        s.push(starting_vertex)
+
+        checked = set()
+
+        while s.size() > 0:
+            current_node = s.pop()
+
+            if current_node not in checked:
+                print(current_node)
+                checked.add(current_node)
+                neighbors = self.get_neighbors(current_node)
+
+                for neighbor in neighbors:
+                    s.push(neighbor)
+
+
+    def dft_recursive(self, starting_vertex, checked=set()):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        This should be done using recursion.
+        """
+
+        if starting_vertex not in checked:
+            checked.add(starting_vertex)
+            print(starting_vertex)
+
+            for neighbor in self.vertices[starting_vertex]:
+                self.dft_recursive(neighbor, checked)
+
+    def bfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing the shortest path from
+        starting_vertex to destination_vertex in
+        breath-first order.
+        """
+        q = Queue()
+        checked = set()
+
+        q.enqueue([starting_vertex])
+
+        while q.size() > 0:
+            path = q.dequeue()
+            node = path[-1]
+
+            if node not in checked:
+                if node == destination_vertex:
+                    return path
+
+                checked.add(node)
+
+                for neighbor in self.vertices[node]:
+                    new_path = path.copy()
+                    new_path.append(neighbor)
+                    q.enqueue(new_path)
+
+    def dfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+        """
+        s = Stack()
+        checked = set()
+
+        s.push([starting_vertex])
+
+        while s.size() > 0:
+            path = s.pop()
+            node = path[-1]
+
+            if node not in checked:
+                if node == destination_vertex:
+                    return path
+
+            checked.add(node)
+
+            for neighbor in self.vertices[node]:
+                new_path = path.copy()
+                new_path.append(neighbor)
+                s.push(new_path)
+
+    def dfs_recursive(self, starting_vertex, destination_vertex, checked=set(), path=[]):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+
+        This should be done using recursion.
+        """
+        checked.add(starting_vertex)
+
+        path = path + [starting_vertex]
+
+        if starting_vertex == destination_vertex:
+            return path
+
+        for neighbor in self.get_neighbors(starting_vertex):
+            if neighbor not in checked:
+                new_path = self.dfs_recursive(neighbor, destination_vertex, checked, path)
+
+                if new_path is not None:
+                    return new_path
+
+
+if __name__ == '__main__':
+    graph = Graph()  # Instantiate your graph
+    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_edge(5, 3)
+    graph.add_edge(6, 3)
+    graph.add_edge(7, 1)
+    graph.add_edge(4, 7)
+    graph.add_edge(1, 2)
+    graph.add_edge(7, 6)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(2, 3)
+    graph.add_edge(4, 6)
+
+    '''
+    Should print:
+        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+    '''
+    print(graph.vertices)
+
+    '''
+    Valid BFT paths:
+        1, 2, 3, 4, 5, 6, 7
+        1, 2, 3, 4, 5, 7, 6
+        1, 2, 3, 4, 6, 7, 5
+        1, 2, 3, 4, 6, 5, 7
+        1, 2, 3, 4, 7, 6, 5
+        1, 2, 3, 4, 7, 5, 6
+        1, 2, 4, 3, 5, 6, 7
+        1, 2, 4, 3, 5, 7, 6
+        1, 2, 4, 3, 6, 7, 5
+        1, 2, 4, 3, 6, 5, 7
+        1, 2, 4, 3, 7, 6, 5
+        1, 2, 4, 3, 7, 5, 6
+    '''
+    graph.bft(1)
+
+    '''
+    Valid DFT paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    graph.dft(1)
+    graph.dft_recursive(1)
+
+    '''
+    Valid BFS path:
+        [1, 2, 4, 6]
+    '''
+    print(graph.bfs(1, 6))
+
+    '''
+    Valid DFS paths:
+        [1, 2, 4, 6]
+        [1, 2, 4, 7, 6]
+    '''
+    print(graph.dfs(1, 6))
+    print(graph.dfs_recursive(1, 6))
